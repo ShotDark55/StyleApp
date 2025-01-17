a7e62a2f527c4f54153d15a0a5e391decaa927d3
= <=>
2025-01-17 12:06:04 +0100

    solved gallery app

--- app/src/main/java/com/okiyacode/styleapplication/GalleryActivity.java
+++ app/src/main/java/com/okiyacode/styleapplication/GalleryActivity.java
@@ -1,8 +1,8 @@
 package com.okiyacode.styleapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,23 +10,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class GalleryActivity extends AppCompatActivity {
 
-    private Button exitBtn,galleryBtn;
+    private ImageView image;
+    private Button next;
+    private final int[] imagePic =  {R.drawable.c,
+            R.drawable.d,
+            R.drawable.e,
+            R.drawable.d,
+            R.drawable.f,
+            R.drawable.g,
+            R.drawable.k,
+            R.drawable.x,
+    };
 
-
-
+    private int index = 0;
     private void setup() {
-        exitBtn = findViewById(R.id.exitID);
-        galleryBtn = findViewById(R.id.buttonGallary);
-    }
+        image = findViewById(R.id.imageViewGallery);
+        next = findViewById(R.id.btnNext);
 
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gallery);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -35,13 +44,15 @@ public class MainActivity extends AppCompatActivity {
 
         setup();
 
-        exitBtn.setOnClickListener(View->{
-            MainActivity.this.finish();
+
+        next.setOnClickListener(v -> {
+            index++;
+            if (index == imagePic.length) {
+                index = 0;
+            }
+            image.setImageResource(imagePic[index]);
         });
 
 
-        galleryBtn.setOnClickListener(View ->{
-            startActivity(new Intent(MainActivity.this,GalleryActivity.class));
-        });
     }
 }
